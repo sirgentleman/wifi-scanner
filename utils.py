ec57ee986b5dd863f0a9aed50c25ce3cf95a1562
sirgentleman <imnotfreeman@gmail.com>
2021-03-20 11:02:48 +0100

    add mac vendor lookup

---
+++ utils.py
@@ -0,0 +1,11 @@
+#! .venv/bin/python3
+
+import requests
+
+_MAC_VENDOR_LOOKUP_URL = "https://macvendors.co/api/vendorname/"
+
+def get_vendor(mac_addr):
+    #TODO: check input format
+
+    response = requests.post(_MAC_VENDOR_LOOKUP_URL+str(mac_addr))
+    return response.text
